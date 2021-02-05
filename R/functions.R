41373708cc5ed2c8fdc9501ab8179a98d06779a0
Jonathan Dayley <32369308+cadnza@users.noreply.github.com>
2021-02-05 11:41:45 -0700

    Initial commit

---
+++ R/functions.R
@@ -0,0 +1,13 @@
+aScriptDisplayDialog <- function(
+	text,
+	defaultAnswer=NA,
+	hiddenAnswer=FALSE,
+	buttons=NA,
+	defaultButton=NA,
+	cancelButton=NA,
+	withTitle=NA,
+	withIcon=NA,
+	givingUpAfter=NA
+){}
+
+aScriptChooseFromList <- function(x){}
